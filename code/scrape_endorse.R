a7556f4c44b73e63f0145590dfd7cee24af12816
Kiernan Nicholls <kiernann@protonmail.com>
2019-06-19 10:04:38 -0400

    Create script to scrape 538 endorsment tracker

---
+++ code/scrape_endorse.R
@@ -0,0 +1,33 @@
+pacman::p_load(
+  tidyverse,
+  lubridate,
+  janitor,
+  rvest,
+  fs
+)
+
+endorse <-
+  read_csv(
+    file = "https://projects.fivethirtyeight.com/endorsements-2020-data/endorsements-2020.csv",
+    col_types = cols(
+      date = col_date(format = ""),
+      position = col_character(),
+      city = col_character(),
+      state = col_character(),
+      endorser = col_character(),
+      endorsee = col_character(),
+      `endorser party` = col_character(),
+      source = col_character(),
+      order = col_double(),
+      category = col_character(),
+      body = col_character(),
+      district = col_double(),
+      points = col_double()
+    )
+  ) %>%
+  clean_names()
+
+write_csv(
+  x = polls,
+  path = here("data", paste(today(), "endorese.csv", sep = "_"))
+)
