430b001ccf5515f2f332882aeeea19b633d067ab
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-06 17:12:00 -0400

    Add code to scrape election results

--- docs/electoral_college.R
+++ docs/electoral_college.R
@@ -6,3 +6,26 @@ p %>%
   filter(name %>% str_detect("Which party will win (.*) in the 2020 presidential election?")) %>%
   mutate(state = shortName %>% str_extract("[:upper:]{2}")) %>%
   select(state, id)
+
+library(rvest)
+page <- read_html("https://en.wikipedia.org/wiki/2016_United_States_presidential_election")
+table <- page %>%
+  html_node("table.wikitable:nth-child(1)") %>%
+  html_table(fill = TRUE) %>%
+  na_if("–") %>%
+  as_tibble(.name_repair = "unique") %>%
+  select(1, 3, 6) %>%
+  slice(-1, -58, -59) %>%
+  set_names(c("state", "dem", "rep")) %>%
+  gather(
+    -state,
+    key = "candidate",
+    value = "votes"
+  ) %>%
+  mutate(votes = parse_number(votes)) %>%
+  arrange(state, candidate)
+
+table %>%
+  group_by(state) %>%
+  arrange(votes) %>%
+  slice(1)
